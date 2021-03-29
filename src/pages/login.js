da8ae4b464758bab095dd10a48b5c19f298d3c4f
Sepp1324 <stoegerer.sebastian@aon.at>
2021-03-30 00:25:06 +0200

    Finished Setup

--- src/pages/login.js
+++ src/pages/login.js
@@ -19,8 +19,16 @@ export default function Login() {
   }, []);
 
   return (
-    <div className='container flex mx-auto max-w-screen-m items-center h-screen'>
-      <p>I have no idea</p>
+    <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
+      <div className='flex w-3/5'>
+        <img
+          src='/images/iphone-with-profile.jpg'
+          alt='iPhone with Instagram app'
+        />
+      </div>
+      <div className='flex flex-col w-2/5'>
+        <p>Form Blyat</p>
+      </div>
     </div>
   );
 }

71fa3b2097815c8b0f29662a1945bb11582e1b05
Sepp1324 <stoegerer.sebastian@aon.at>
2021-03-29 20:52:39 +0200

    Setup Done

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// client side rendered app: react (cra)
+// -> database (firebase)
+// -> react-loading-skeleton
+// tailwind
+
+// folder structure
+// src
+// -> components, constants, context, helpers, hooks, lib (firebase), services(firebase functions), styles(tailwind)

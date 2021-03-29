09530eef9d95f2650343bd696e2642aa7d53f51b
Sepp1324 <stoegerer.sebastian@aon.at>
2021-03-30 00:11:55 +0200

    Setup for Tailwind

---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { seedDatabase } from '../seed';
+
+const config = {
+  apiKey: 'AIzaSyDa8kFPAGUx1DH_7-ixZEdKXwJakSKfwAk',
+  authDomain: 'instagram-ae1e7.firebaseapp.com',
+  projectId: 'instagram-ae1e7',
+  storageBucket: 'instagram-ae1e7.appspot.com',
+  messagingSenderId: '808455691114',
+  appId: '1:808455691114:web:05e5f176f39dd53ca6a313'
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+console.log('firebase', firebase);
+
+// Call seed-file once
+// seedDatabase(firebase);
+
+export { firebase, FieldValue };
---
+++ src/pages/login.js
@@ -0,0 +1,26 @@
+import { useContext, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
+export default function Login() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, seteEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
+
+  return (
+    <div className='container flex mx-auto max-w-screen-m items-center h-screen'>
+      <p>I have no idea</p>
+    </div>
+  );
+}

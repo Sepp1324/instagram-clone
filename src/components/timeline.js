06ea6614c3ca2c689fb5e1080c1212836c3961d3
Sepp1324 <stoegerer.sebastian@aon.at>
2021-03-30 19:47:25 +0200

    Worked on basic timeline and post rendering

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,28 @@
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+
 export default function Timeline() {
+  // get users photos from user logged in (hook)
+  const { photos } = usePhotos();
+
+  console.log(`photos`, photos);
+  // on loading the photos, need react skeleton
+  // if we have photos -> render them (create a post component)
+  // if user has no photos, tell them to create photos
+
   return (
     <div className='container col-span-2'>
-      <p>Timeline</p>
+      {!photos ? (
+        <>
+          <Skeleton count={4} width={640} height={500} className='mb-5' />)
+        </>
+      ) : photos.length > 0 ? (
+        photos.map(content => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className='text-center text-2xl'>
+          Follow people to see their posts!
+        </p>
+      )}
     </div>
   );
 }

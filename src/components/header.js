3acd36d3e5a429e964516a031c394afe10c26b1c
Sepp1324 <stoegerer.sebastian@aon.at>
2021-04-03 00:44:39 +0200

    Fixed: When clicking on logout, redirect to login page

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import UserContext from '../context/user';
 import * as ROUTES from '../constants/routes';
@@ -8,6 +8,8 @@ export default function Header() {
   const { firebase } = useContext(FirebaseContext);
   const { user } = useContext(UserContext);
 
+  const history = useHistory();
+
   return (
     <header className='h-16 bg-white border-b border-gray-primary mb-8'>
       <div className='container mx-auto max-w-screen-lg h-full'>
@@ -15,11 +17,7 @@ export default function Header() {
           <div className='text-gray-700 text-center flex items-center align-items cursor-pointer'>
             <h1 className='flex justify-center w-full'>
               <Link to={ROUTES.DASHBOARD} aria-label='Instagram logo'>
-                <img
-                  src='/images/logo.png'
-                  alt='Instagram'
-                  className='mt-2 w-6/12'
-                />
+                <img src='/images/logo.png' alt='Instagram' className='mt-2 w-6/12' />
               </Link>
             </h1>
           </div>
@@ -48,12 +46,12 @@ export default function Header() {
                   title='Sign Out'
                   onClick={() => {
                     firebase.auth().signOut();
-                    // history.push(ROUTES.LOGIN);
+                    history.push(ROUTES.LOGIN);
                   }}
                   onKeyDown={event => {
                     if (event.key === 'Enter') {
                       firebase.auth().signOut();
-                      // history.push(ROUTES.LOGIN);
+                      history.push(ROUTES.LOGIN);
                     }
                   }}
                 >
@@ -86,18 +84,12 @@ export default function Header() {
             ) : (
               <>
                 <Link to={ROUTES.LOGIN}>
-                  <button
-                    type='button'
-                    className='bg-blue-medium font-bold text-sm rounded text-white w-20 h-8'
-                  >
+                  <button type='button' className='bg-blue-medium font-bold text-sm rounded text-white w-20 h-8'>
                     Log In
                   </button>
                 </Link>
                 <Link to={ROUTES.SIGN_UP}>
-                  <button
-                    type='button'
-                    className='font-bold text-sm rounded text-blue-medium w-20 h-8'
-                  >
+                  <button type='button' className='font-bold text-sm rounded text-blue-medium w-20 h-8'>
                     Sign Up
                   </button>
                 </Link>
--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -6,11 +6,9 @@ export default function usePhotos(user) {
 
   useEffect(() => {
     async function getTimelinePhotos() {
-      let followedUserPhotos = [];
-
       // does user actually follow people?
       if (user?.following.length > 0) {
-        followedUserPhotos = await getPhotos(user.userId, user.following);
+        const followedUserPhotos = await getPhotos(user.userId, user.following);
 
         followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated); // newest photos on top of feed
         setPhotos(followedUserPhotos);

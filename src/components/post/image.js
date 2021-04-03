4e2d5187a85f28676e6535618756d2b836ec67e7
Sepp1324 <stoegerer.sebastian@aon.at>
2021-04-03 02:08:34 +0200

    Fixed default Images on Timeline

--- src/components/post/image.js
+++ src/components/post/image.js
@@ -1,7 +1,16 @@
 import PropTypes from 'prop-types';
+import { DEFAULT_IMAGE_PATH } from '../../constants/paths';
 
 export default function Image({ src, caption }) {
-  return <img src={src} alt={caption} />;
+  return (
+    <img
+      src={src}
+      alt={caption}
+      onError={event => {
+        event.target.src = DEFAULT_IMAGE_PATH;
+      }}
+    />
+  );
 }
 
 Image.propTypes = {
